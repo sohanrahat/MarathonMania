df073692c7c9e7c088612b4ff5bd3d816c6718c2
sohanrahat <sohanboy@gmail.com>
2025-06-08 12:38:49 +0600

    Root Layout Setup

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element:
+  },
+    {
+        path: "/about",
+        element: <About />
+    }
+])
