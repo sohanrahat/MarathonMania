00ffa6b6c1c65210e134d687b0e6ab74a86db40f
sohanrahat <sohanboy@gmail.com>
2025-06-08 12:42:20 +0600

    Root Layout defined

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Navbar from '../Components/Navbar';
+import { Outlet } from 'react-router';
+import Footer from '../Components/Footer';
 
 const Root = () => {
     return (
         <div>
-
+            <nav>
+                <Navbar />
+            </nav>
+            <main>
+                <Outlet />
+            </main>
+            <footer>
+                <Footer />
+            </footer>
         </div>
     );
 };
