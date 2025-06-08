a7f567109b2cf0be0557cae67382c3a39ca09067
sohanrahat <sohanboy@gmail.com>
2025-06-08 14:11:05 +0600

    router setup in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import './styles/colors.css'
+import { RouterProvider } from 'react-router'
+import router from './Routes/Router.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
