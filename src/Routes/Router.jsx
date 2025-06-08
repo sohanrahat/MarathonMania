813f770c608e1c66f60ba1135dac35d28d36e98f
sohanrahat <sohanboy@gmail.com>
2025-06-08 12:47:09 +0600

    Router Re Setup

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,12 +1,21 @@
 import { createBrowserRouter } from "react-router";
+import Root from "../Layouts/Root";
+import ErrorPage from "../Pages/ErrorPage";
+import Home from "../Pages/Home";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element:
-  },
-    {
-        path: "/about",
-        element: <About />
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            },
+
+        ]
     }
-])
+]);
+
+export default router;
