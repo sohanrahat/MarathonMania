4f2eccff658f68080c8e12cadcd53a8c2f006628
sohanrahat <sohanboy@gmail.com>
2025-06-14 18:18:29 +0600

    improved dashboard Layout

--- src/Dashboard/DashboardLayout.jsx
+++ src/Dashboard/DashboardLayout.jsx
@@ -4,13 +4,15 @@ import DashNav from '../Components/DashNav';
 
 const DashboardLayout = () => {
     return (
-        <div className="grid grid-cols-5">
-            <div className="col-span-1">
-                <DashNav />
+        <div className="container mx-auto my-8">
+            <div className="grid grid-cols-5 gap-6 bg-white rounded-lg shadow-md">
+                <div className="col-span-1  min-h-screen bg-neutral-light p-4">
+                    <DashNav />
+                </div>
+                <main className="col-span-4 p-6">
+                    <Outlet />
+                </main>
             </div>
-            <main className="col-span-4 p-6">
-                <Outlet />
-            </main>
         </div>
     );
 };
