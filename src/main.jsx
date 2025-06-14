88ddd9df5e9bc4cf78ff86f1e3bd0adaf627a5a0
sohanrahat <sohanboy@gmail.com>
2025-06-14 13:52:57 +0600

    AuthProvider Implementation in mainsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import './index.css'
 import './styles/colors.css'
 import { RouterProvider } from 'react-router'
 import router from './Routes/Router.jsx'
+import AuthProvider from './Context/AuthProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>,
 )
